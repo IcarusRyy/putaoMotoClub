40f1606688db82382ce0dd6e3ccc02083c8c90f1
apple <apple@MacBook-Pro.local>
2022-08-15 15:07:01 +0800

    配置sass、设置页面标题logo、整理文件目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+// import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+    alias: {
+      "@": "/src",
+    },
+  },
+  css: {
+    // css 预处理器
+    preprocessorOptions: {
+      scss: {
+        /*
+				引入var.scss全局预定义变量，
+				如果引入多个文件，
+				可以使用
+				'@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
+				这种格式
+				 */
+        additionalData: `@import "@/styles/global.scss";`,
+      },
+    },
+  },
 })
